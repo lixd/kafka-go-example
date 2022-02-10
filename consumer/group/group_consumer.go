6be1900cdba22ed9a6dfbb850a8d0580aeb16b0d
lixd <xueduan.li@gmail.com>
2022-02-10 18:51:55 +0800

    fix: rebalance 问题

--- consumer/group/group_consumer.go
+++ consumer/group/group_consumer.go
@@ -68,10 +68,12 @@ func ConsumerGroup(topic, group, name string) {
 		for {
 			fmt.Println("running: ", name)
 			/*
+				![important]
 				应该在一个无限循环中不停地调用 Consume()
 				因为每次 Rebalance 后需要再次执行 Consume() 来恢复连接
 				Consume 开始才发起 Join Group 请求 如果当前消费者加入后成为了 消费者组 leader,则还会进行 Rebalance 过程，从新分配
 				组内每个消费组需要消费的 topic 和 partition，最后 Sync Group 后才开始消费
+				具体信息见 https://github.com/lixd/kafka-go-example/issues/4
 			*/
 			err = cg.Consume(ctx, []string{topic}, handler)
 			if err != nil {
